b6c4fa5a07422dc8ad9b9971e3ae6252726b2a6e
joaobiriba <glaquidara@gmail.com>
2021-02-26 15:20:22 +0000

    Complete challenge

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,13 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.model.puppiesList
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,8 +41,25 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "home") {
+        composable("home") {
+            PuppiesHome(
+                puppies = puppiesList,
+                navigateToPuppyDetails = { puppy ->
+                    navController.navigate("puppy/${puppy.name}")
+                }
+            )
+        }
+        composable("puppy/{name}") { backStackEntry ->
+            val puppyName = backStackEntry.arguments?.getString("name")
+            val puppy = puppiesList.find { it.name == puppyName }
+                ?: throw IllegalStateException("puppy $puppyName not found")
+            PuppyDetails(
+                puppy = puppy,
+                navigateBack = { navController.popBackStack() }
+            )
+        }
     }
 }
 
